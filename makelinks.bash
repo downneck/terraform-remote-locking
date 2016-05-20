5d39acdca452b1b761b3b567ed3fe3dc5074daa1
David Kovach <dkovach@jibe.com>
2016-05-20 11:01:35 -0400

    adding license and copyright info

--- makelinks.bash
+++ makelinks.bash
@@ -1,7 +1,23 @@
 #!/bin/bash
 #
+# makelinks.bash, Copyright 2016 David Kovach and William Broach
+#
 # walks through all the subdirectories of the main repo dir and creates
 # links back to common files necessary for a terraform stack to operate
+#
+# This is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this.  If not, see <http://www.gnu.org/licenses/>.
+
 
 # make sure we have a TF_STATE_BUCKET env var populated
 if [ -z "$TF_STATE_BUCKET" ]; then
